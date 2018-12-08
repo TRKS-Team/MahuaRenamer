2578155e92b37a85defd1e48c3605b345fda44c5
Cyl18 <cyl18a@gmail.com>
2018-12-08 17:15:55 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MahuaRenamer
+{
+    static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var buildNumber = Environment.GetEnvironmentVariable("APPVEYOR_BUILD_NUMBER", EnvironmentVariableTarget.Machine);  
+            var directories = Directory.GetDirectories(".", "MahuaEvents", SearchOption.AllDirectories);
+            if (directories.Length != 1) throw new FileNotFoundException("We could not find one 'MahuaEvents' folder.");
+            if (buildNumber is null) throw new NotSupportedException("Please run this program in AppVeyor environment.");
+
+            var classNames = Directory.GetFiles(directories.First()).Select(Path.GetFileNameWithoutExtension).ToArray();
+            var files = Directory.GetFiles(".","*.cs", SearchOption.AllDirectories);
+            for (var i = 0; i < files.Length; i++)
+            {
+                var file = files[i];
+                Console.WriteLine($"[{i}/{files.Length}] Processing: {Path.GetFileName(file)}");
+                var content = File.ReadAllText(file);
+                content = classNames.Aggregate(content, (current, name) => current.Replace(name, $"{name}_{buildNumber}"));
+                File.WriteAllText(file, content, Encoding.UTF8);
+            }
+
+            Console.WriteLine("Operation finished.");
+        }
+    }
+}
